835f16354f6ed4626346d93c821164f5cc528d33
Olivier Favre <of.olivier.favre@gmail.com>
2011-12-10 17:30:14 +0100

    PageExtractor.Algo.XPath.Query: XPath query generation
    Takes {depth, depth_reversed, tag, classes, position} n-uples.
    tag and position can be undefined
    classes can be an empty array.
    
    Works either with the depth or the reversed depth.
    
    Can go back up N parents.
    
    Can be ORed.
    
    NOTE:
    * Copy does not copy the ORed queries, only the current part.
    
    Next step:
    * Using this in Algo.learn() for the first query and recursively.

--- src/main/algo/xpath.js
+++ src/main/algo/xpath.js
@@ -19,6 +19,161 @@ window.PageExtractor.Algo.XPath.getResults = function (xpath) {
     return rtn;
 }
 
+window.PageExtractor.Algo.XPath.Query = function() {
+    /*
+     * Multiple ORed queries (using "|").
+     * ANDing is done implicitly when writing a query.
+     * Input criteria is of the form:
+     *    {Depth (reversed?), tag or "*", classes or empty, optional position} + ancestry level
+     */
+    var orQueries = [];
+    this.addOr = function (query) {
+        orQueries.push(query);
+        return this;
+    };
+    var useReversedDepth = true;
+    this.setUseReversedDepth = function (value) {
+        useReversedDepth = value ? true : false;
+        return this;
+    };
+    var criteria = [
+        // {
+        //   depth: a positive (from root) number,
+        //   depth_reversed: a negative (from target) number,
+        //   tag: "SOMETAG" or undefined/"*",
+        //   classes: [array of class, or empty],
+        //   position: a positive (from first()) or negative (from last()) number or undefined
+        // }
+    ];
+    var criteriaByDepth = {
+        // depth: criteria index
+    };
+    var criteriaByReversedDepth = {
+        // reversed depth: criteria index
+    };
+    this.addCriterion = function(depth, depth_reversed, tag, classes, position) {
+        // New criterion
+        var newCriterion = {
+            depth: depth,
+            depth_reversed: depth_reversed,
+            tag: tag,
+            classes: classes,
+            position: position
+        };
+        // Look for depth to be already known
+        var id = criteriaByDepth[depth];
+        if (id == undefined) {
+            // Create one new id
+            // Index it
+            criteriaByDepth[depth] = criteria.length;
+            // Add the criterion
+            criteria.push(newCriterion);
+        } else
+            criteria[id] = newCriterion;
+        // Look for depth_reversed to be already known
+        id = criteriaByReversedDepth[depth_reversed];
+        if (id == undefined) {
+            // Create one new id
+            // Index it
+            criteriaByReversedDepth[depth_reversed] = criteria.length;
+            // Add the criterion
+            criteria.push(newCriterion);
+        } else
+            criteria[id] = newCriterion;
+    };
+    var ancestryLevel = 0; // How many time to go back up from the target XPath element to reach the actual desired element
+    this.setAncestryLevel = function(value) {
+        ancestryLevel = parseInt(value);
+        if (isNaN(ancestryLevel))
+            ancestryLevel = 0;
+        return this;
+    };
+    this.toXPath = function() {
+        var prefix = "/html/body";
+        var parts = [];
+        if (criteria.length > 0) {
+            /*
+             * Generate string for each depth
+             *   constrained?
+             *     generate the constrain
+             *   unconstrained
+             *     "*"
+             * Join with "/"
+             * Padding:
+             *   !reversed
+             *     prepend N times "*"
+             *   reversed?
+             *     append N times "*"
+             * Bootstrap:
+             *   reversed?
+             *     prepend "/" (an additional / to get '//')
+             */
+            var set = useReversedDepth ? criteriaByReversedDepth : criteriaByDepth;
+            for (var key in set) {
+                var crit = criteria[set[key]];
+                var depth = useReversedDepth ? Math.abs(crit.depth_reversed) : crit.depth;
+                var preconditions = [];
+                if (crit.position != undefined) {
+                    if (crit.position >= 0)
+                        preconditions.push("position()="+crit.position);
+                    else
+                        preconditions.push("position()=last()-"+Math.abs(crit.position));
+                }
+                if (crit.classes.length > 0)
+                    preconditions.push('contains(concat(" ",@class," ")," ' + crit.classes.join(' ") and contains(concat(" ",@class," ")," ') + ' ")');
+                var part = crit.tag == undefined ? "*" : crit.tag;
+                if (preconditions.length > 0)
+                    part += "["+preconditions.join(" and ")+"]";
+                parts[depth] = part;
+            }
+            for (var i = 1 ; i < parts.length ; i++)
+                if (parts[i] == undefined)
+                    parts[i] = "*";
+            if (useReversedDepth) {
+                parts.reverse();
+                // Keep the first element at the first place, it's a placeholder
+                parts.unshift(parts.pop());
+            }
+            if (useReversedDepth)
+                prefix += "/";
+        }
+        parts[0] = prefix;
+        // Get the nth ancestor of the target element
+        for (var i = Math.abs(ancestryLevel) ; i > 0 ; i--) {
+            if (parts[parts.length-1] == "*")
+                parts.splice(parts.length-1,1);
+            else
+                parts.push("..");
+        }
+        if (ancestryLevel == 0)
+            parts.push(".");
+        var rtn = parts.join("/");
+        // ORing with other parts and returning
+        if (orQueries.length > 0) {
+            var ors = [];
+            if (rtn != "")
+                ors.push(rtn);
+            for (var i = 0 ; i < orQueries.length ; i++) {
+                var sub = orQueries[i].toXPath();
+                if (sub.length != 0)
+                    ors.push(sub);
+            }
+            rtn = ors.join(" | ");
+        }
+        return rtn;
+    };
+    this.copySelf = function() { // Returns a copy of the current query, WITHOUT the ORed queries
+        var rtn = new window.PageExtractor.Algo.XPath.Query();
+        rtn.setAncestryLevel(ancestryLevel);
+        rtn.setUseReversedDepth(useReversedDepth);
+        for (var i = 0 ; i < criteria.length ; i++) {
+            var c = criteria[i];
+            rtn.addCriterion(c.depth, c.depth_reversed, c.tag, c.classes, c.position);
+        }
+        return rtn;
+    };
+}
+
 window.PageExtractor.Algo.XPath.makeXPath = function (target) {
     var parts = [];
     var curr = target;
